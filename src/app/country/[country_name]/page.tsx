bec0548ed9b78e3b750154b36bd4d26279a80891
HamzaRauf1947 <hamzaraufstudy@gmail.com>
2024-10-04 17:07:46 +0500

    fixed

--- src/app/country/[country_name]/page.tsx
+++ src/app/country/[country_name]/page.tsx
@@ -1,11 +1,11 @@
-'use client'
+'use client';
 import Button from '@/app/component/Button';
 import countries from '@/app/data/countries';
 import Link from 'next/link';
 
-export default function CountryDetails({ params }) {
-    const country = countries.find((c) => c.name.toLowerCase() === params.country_name.toLowerCase());
-
+export default function CountryDetails({ params }: { params: { country_name: string } }) {
+    const countryName = params.country_name;
+    const country = countries.find((c) => c.name.toLowerCase() === countryName.toLowerCase());
 
     if (!country) {
         return (
@@ -20,21 +20,19 @@ export default function CountryDetails({ params }) {
     }
 
     return (
-        <>
         <div 
-        className="flex items-center justify-center h-screen" 
-        style={{ backgroundImage: `url(${country.image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
-    >
-        <div className="bg-white bg-opacity-80 p-4 rounded-lg shadow-lg text-center">
-            <h1 className="text-3xl font-bold mb-4">{country.name}</h1>
-            <p className="text-lg mb-2">Population: <span className="font-semibold">{country.population.toLocaleString()}</span></p>
-            <p className="text-lg mb-2">Capital: <span className="font-semibold">{country.capital}</span></p>
-            <Link href="/country" className="text-blue-700 hover:underline text-lg">
-                Go back to Country List
-            </Link>
-        < Button/>
+            className="flex items-center justify-center h-screen" 
+            style={{ backgroundImage: `url(${country.image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+        >
+            <div className="bg-white bg-opacity-80 p-4 rounded-lg shadow-lg text-center">
+                <h1 className="text-3xl font-bold mb-4">{country.name}</h1>
+                <p className="text-lg mb-2">Population: <span className="font-semibold">{country.population.toLocaleString()}</span></p>
+                <p className="text-lg mb-2">Capital: <span className="font-semibold">{country.capital}</span></p>
+                <Link href="/country" className="text-blue-700 hover:underline text-lg">
+                    Go back to Country List
+                </Link>
+                <Button />
+            </div>
         </div>
-    </div>
-        </>
     );
 }
