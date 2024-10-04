5dc5c1ba85a63d3de6d107a4b534805aef698fd2
HamzaRauf1947 <hamzaraufstudy@gmail.com>
2024-10-04 17:38:38 +0500

    fixed margin

--- src/app/component/Button.tsx
+++ src/app/component/Button.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Button() {
   return (
-    <div className="flex justify-center m-4">
+    <div className="flex justify-center mt-4 pb-4">
       <Link 
         href="/" 
         className="bg-blue-600 text-white font-bold py-2 px-6 rounded-lg shadow-lg transition-transform transform hover:scale-105 hover:bg-blue-700"
